7063fe070a047e6de2c078e964d706fec583c575
Nikolay <rachkuw@gmail.com>
2021-08-31 15:05:13 +0300

    new module icon

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $config = array();
 $config['name'] = "Vimeo Videos Playlist";
 $config['author'] = "Petko Yovchevski";
 $config['ui'] = true;
-$config['version'] = 0.4;
+$config['version'] = 0.5;
 $config['position'] = 57;
